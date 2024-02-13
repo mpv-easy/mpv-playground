900c55ece39cca3fab7741fb3537c1081bf9af80
ahaoboy <504595380@qq.com>
2024-02-13 17:01:28 +0800

    init

---
+++ src/main.tsx
@@ -0,0 +1,4 @@
+import React from 'react'
+import Element from './index'
+import { renderToBrowser } from './renderToBrowser'
+renderToBrowser(<Element />)
---
+++ src/renderToMpv.tsx
@@ -0,0 +1,8 @@
+import "@mpv-easy/polyfill"
+import "@mpv-easy/tool"
+import React from "react"
+import { render } from "@mpv-easy/ui"
+
+import Element from './index'
+
+render(<Element />)
